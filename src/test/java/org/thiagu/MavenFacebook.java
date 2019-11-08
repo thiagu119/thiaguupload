c711ada0368a2cf2dfac9da1ec734b74085e0fcf
thiagarajan <thiagu119@gmail.com>
2019-11-08 11:30:10 +0530

    Hari now added something

--- src/test/java/org/thiagu/MavenFacebook.java
+++ src/test/java/org/thiagu/MavenFacebook.java
@@ -1,6 +1,11 @@
 package org.thiagu;
 
 public class MavenFacebook {
+	
+	public void student() {
+System.out.println("My Name is Khan");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
